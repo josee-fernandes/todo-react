710d48b96bc3bfc945b2fdbaa261322c47e7b6b9
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2023-12-29 23:55:49 -0300

    Check toggle + align container

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
+import { Check } from "phosphor-react";
+
 import styles from "./Task.module.css";
 
 export function Task() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleToggleIsChecked() {
+    setIsChecked((oldIsChecked) => !oldIsChecked);
+  }
+
   return (
     <li className={styles.task}>
-      <div>
-        <button className={styles.check}></button>
+      <div className={styles.checkboxWrapper}>
+        <input type="checkbox" checked={isChecked} readOnly />
+        <button className={styles.check} onClick={handleToggleIsChecked}>
+          {isChecked && <Check />}
+        </button>
       </div>
       <p>
         Integer urna interdum massa libero auctor neque turpis turpis semper.
