0857da381e3aa5357c538b3b30e058576b446bf4
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-01-02 20:23:17 -0300

    Create new task functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,58 @@ import { Header } from "./components/Header";
 import { NewTask } from "./components/NewTask";
 import { TaskType, Tasks } from "./components/Tasks";
 
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 
 import "./global.css";
 
 import styles from "./App.module.css";
+import { useState } from "react";
 
-const tasks: TaskType[] = [
-  {
-    id: uuidv4(),
-    content:
-      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    isChecked: false,
-  },
-  {
-    id: uuidv4(),
-    content:
-      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    isChecked: false,
-  },
-  {
-    id: uuidv4(),
-    content:
-      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    isChecked: false,
-  },
-];
+// const tasks: TaskType[] = [
+//   {
+//     id: uuidv4(),
+//     content:
+//       "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+//     isChecked: false,
+//   },
+//   {
+//     id: uuidv4(),
+//     content:
+//       "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+//     isChecked: false,
+//   },
+//   {
+//     id: uuidv4(),
+//     content:
+//       "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+//     isChecked: false,
+//   },
+//   {
+//     id: uuidv4(),
+//     content:
+//       "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+//     isChecked: true,
+//   },
+//   {
+//     id: uuidv4(),
+//     content:
+//       "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+//     isChecked: true,
+//   },
+// ];
 
 function App() {
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  function createTask(task: TaskType) {
+    setTasks((oldTasks) => [...oldTasks, task]);
+  }
+
   return (
     <div>
       <Header />
       <div className={styles.container}>
-        <NewTask />
+        <NewTask onCreateTask={createTask} />
         <Tasks tasks={tasks} />
       </div>
     </div>
--- src/components/NewTask/index.tsx
+++ src/components/NewTask/index.tsx
@@ -1,11 +1,44 @@
 import { PlusCircle } from "phosphor-react";
+import { v4 as uuidv4 } from "uuid";
 
 import styles from "./NewTask.module.css";
+import { TaskType } from "../Tasks";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-export function NewTask() {
+interface NewTaskProps {
+  onCreateTask: (task: TaskType) => void;
+}
+
+export function NewTask({ onCreateTask }: NewTaskProps) {
+  const [newTaskContent, setNewTaskContent] = useState("");
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskContent(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const newTask: TaskType = {
+      id: uuidv4(),
+      content: newTaskContent,
+      isChecked: false,
+    };
+
+    onCreateTask(newTask);
+
+    setNewTaskContent("");
+  }
+
   return (
-    <form className={styles.newTask}>
-      <input type="text" placeholder="Adicione uma nova tarefa" />
+    <form className={styles.newTask} onSubmit={handleCreateNewTask}>
+      <input
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+        value={newTaskContent}
+        onChange={handleNewTaskChange}
+        required
+      />
       <button>
         Criar <PlusCircle size={16} />
       </button>
